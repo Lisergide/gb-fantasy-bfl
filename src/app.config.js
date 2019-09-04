458680531bc35f8bbab2a66b7a180808cd809a94
Лисичкин Борис Александрович <BALisichkin@alfabank.ru>
2019-09-04 13:55:16 +0300

    init .env file

--- src/app.config.js
+++ src/app.config.js
@@ -2,5 +2,5 @@ export default {
   url: 'https://dev-823877.okta.com',
   issuer: 'https://dev-823877.okta.com/oauth2/default',
   redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oa15s3ezwxEqDVmH357'
+  client_id: process.env.OKTA_CLIENT_ID
 }
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCNzjKgf8fD4KMYm_DR7rLHRgF9PFcElOk",
+  apiKey: process.env.FIREBASE_APIKEY,
   authDomain: "gb-fantasy-bfl.firebaseapp.com",
   databaseURL: "https://gb-fantasy-bfl.firebaseio.com",
   projectId: "gb-fantasy-bfl",
   storageBucket: "gb-fantasy-bfl.appspot.com",
-  messagingSenderId: "1025436061529",
-  appId: "1:1025436061529:web:3ea1fc450f94fa42"
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
